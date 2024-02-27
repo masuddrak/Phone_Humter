d330a59608fe1d29f617586c48ff28112bf43919
masud <masudranaacb@gmail.com>
2024-02-28 01:58:32 +0600

    added card

--- phones.js
+++ phones.js
@@ -1,14 +1,30 @@
 
 const allPhone = async () => {
-   const res= await fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
-   const data=await res.json()
-   displayPhones(data)
+    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+    const data = await res.json()
+    displayPhones(data)
 }
-const displayPhones=(data)=>{
-    const phones=data.data
+const displayPhones = (data) => {
+    const phones = data.data
+    const card_container = document.getElementById("card_container")
     phones.forEach(phone => {
-        console.log(phone.phone_name)
+        // console.log(phone.phone_name)
+
+        // let createPhone = document.createElement("div")
+        // // div.classList(`card w-96 bg-base-100 shadow-xl`)
+        // createPhone.innerHTML = `
+        // <figure><img src="${phone.image}" />
+        //         </figure>
+        //         <div class="card-body">
+        //             <h2 class="card-title">${phone.phone_name}</h2>
+        //             <p>If a dog chews shoes whose shoes does he choose?</p>
+        //             <div class="card-actions justify-end">
+        //                 <button class="btn btn-primary">Buy Now</button>
+        //             </div>
+        //         </div>
+        // `
+        // card_container.appendChild(createPhone)
     });
-  
+
 }
 allPhone()
